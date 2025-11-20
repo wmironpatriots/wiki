a59fd11e6ecef801b0ebcfbd97c0b1e987f81f29
Srihari <srihariduraivan@gmail.com>
2025-11-19 22:29:38 -0500

    mod: modification to fibonacci calculator
    Signed-off-by: Srihari <srihariduraivan@gmail.com>

--- onboarding-A/easyTasks/FibonacciCalculator.java
+++ onboarding-A/easyTasks/FibonacciCalculator.java
@@ -5,23 +5,20 @@ public class FibonacciCalculator {
             long[] fibNumbers = new long[n];
     
      
-            if (n >= 1) {
-                fibNumbers[0] = 0;
-            }
-            if (n >= 2) {
-                fibNumbers[1] = 1;
-            }
+            
+            fibNumbers[0] = 0;
+            fibNumbers[1] = 1;
+            
     
      
             for (int i = 2; i < n; i++) {
                 fibNumbers[i] = fibNumbers[i - 1] + fibNumbers[i - 2];
+               
             }
-    
-            
-            System.out.println("First " + n + " Fibonacci numbers:");
-            for (int i = 0; i < n; i++) {
-                System.out.println("F(" + i + ") = " + fibNumbers[i]);
+            for (int i = 0; i < 64; i++)
+            {
+              System.out.println(fibNumbers[i]);
             }
         }
     }
-
+   
