a8cb1861656d7723ec75eb96f3c2a833ac243d9d
bzhan <bzhang5460@gmail.com>
2025-11-22 19:37:03 -0500

    fix: Fibonacci now correctly computes 64 numbers instead of 65 bc bobby is dumb
    Signed-off-by: bzhan <bzhang5460@gmail.com>

--- onboarding-A/easyTasks/Fibonacci.java
+++ onboarding-A/easyTasks/Fibonacci.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Fibonacci {
     public static void main(String[] args){
         ArrayList<Long> fibonacci = new ArrayList<>(List.of(0L, 1L));
-        for (int i = 2; i < 64 + 1; i++) {
+        for (int i = 2; i < 64; i++) {
             Long previous = fibonacci.get(i-1);
             Long lastprevious = fibonacci.get(i-2);
             fibonacci.add(previous + lastprevious);
