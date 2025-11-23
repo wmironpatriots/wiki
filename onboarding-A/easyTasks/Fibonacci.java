8d725d3d0a76bd997508ecf42a2e8b7d4e403734
bzhan <bzhang5460@gmail.com>
2025-11-22 19:31:00 -0500

    fix: Fibonacci no longer overflows
    Signed-off-by: bzhan <bzhang5460@gmail.com>

--- onboarding-A/easyTasks/Fibonacci.java
+++ onboarding-A/easyTasks/Fibonacci.java
@@ -3,12 +3,14 @@ import java.util.List;
 
 public class Fibonacci {
     public static void main(String[] args){
-        ArrayList<Integer> fibonacci = new ArrayList<>(List.of(0, 1));
-        for (int i = 2; i < 100; i++) {
-            int previous = fibonacci.get(i-1);
-            int lastprevious = fibonacci.get(i-2);
+        ArrayList<Long> fibonacci = new ArrayList<>(List.of(0L, 1L));
+        for (int i = 2; i < 64 + 1; i++) {
+            Long previous = fibonacci.get(i-1);
+            Long lastprevious = fibonacci.get(i-2);
             fibonacci.add(previous + lastprevious);
         }
-        System.out.println(fibonacci);
+        for (Long num : fibonacci){
+            System.out.println(num);
+        }
     }
 }
