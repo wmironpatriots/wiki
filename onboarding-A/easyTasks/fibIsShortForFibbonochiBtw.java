cd333bceeca2096cbc443846a2aa9737d170e6c6
macetrap <macetrap@gmail.com>
2025-12-07 19:52:18 -0500

    added println and fixed variables

--- onboarding-A/easyTasks/fibIsShortForFibbonochiBtw.java
+++ onboarding-A/easyTasks/fibIsShortForFibbonochiBtw.java
@@ -1,7 +1,8 @@
 public class MyProgram {
     public static void main(String[] args) {
         int n = 64;
-        long fib1 = 0, fib2 = 1; # what is wrong here tho
+        long fib1 = 0;
+	long fib2 = 1; 
 
         for (int i = 1; i <= n; ++i) {
             System.out.println(fib1);
