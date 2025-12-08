64f1df1f08a2e9c36fcfb232096d30d9bd7f756e
macetrap <macetrap@gmail.com>
2025-12-07 19:46:14 -0500

    added println

--- onboarding-A/easyTasks/fibIsShortForFibbonochiBtw.java
+++ onboarding-A/easyTasks/fibIsShortForFibbonochiBtw.java
@@ -1,10 +1,10 @@
 public class MyProgram {
     public static void main(String[] args) {
         int n = 64;
-        long fib1 = 0, fib2 = 1;
+        long fib1 = 0, fib2 = 1; # what is wrong here tho
 
         for (int i = 1; i <= n; ++i) {
-            System.out.print(fib1 + "\n");
+            System.out.println(fib1);
             long sum = fib1 + fib2;
             fib1 = fib2;
             fib2 = sum;
